a6e0ddcb400c63fccc399aa770317ab779aa3a9b
Gary Stofer <GaryStofer@gmail.com>
2018-10-30 15:47:41 -0700

    moved NanoModemAX25 to its own repo  to APRS_NanoModem

--- NanoModemAX25/AVRDUDE_standalone_Flash/bin/2flashAPRS.cmd
+++
@@ -1,1 +0,0 @@
-avrdude -v -p m328p -c arduino -PCOM8 -b57600   -U flash:w:..\..\Software\MicroAPRS\precompiled\microaprs-5v-ss-latest.hex:i
--- NanoModemAX25/AVRDUDE_standalone_Flash/bin/flashAPRS.bat
+++
@@ -1,2 +0,0 @@
-rem Change port, baud and filename to suit
-avrdude -v -p m328p -c arduino -PCOM5 -b57600   -U flash:w:C:\tmp\Modem.hex:i
